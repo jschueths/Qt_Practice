8a55cc5eb51d1773777db453b8d0e5b335f9ca27
Joshua McCarville-Schueths <joshua.mccarville-schueths@maryville.com>
2012-05-25 15:57:33 -0500

    Changed readme file extension

---
+++ TextFinder_Qt_example/TextFinder-build-desktop-Qt_4_7_4_for_Desktop_-_MinGW_4_4__Qt_SDK__Debug/ui_textfinder.h
@@ -0,0 +1,99 @@
+/********************************************************************************
+** Form generated from reading UI file 'textfinder.ui'
+**
+** Created: Fri May 25 14:14:27 2012
+**      by: Qt User Interface Compiler version 4.7.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_TEXTFINDER_H
+#define UI_TEXTFINDER_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QPushButton>
+#include <QtGui/QTextEdit>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_TextFinder
+{
+public:
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QPushButton *findButton;
+    QTextEdit *textEdit;
+
+    void setupUi(QWidget *TextFinder)
+    {
+        if (TextFinder->objectName().isEmpty())
+            TextFinder->setObjectName(QString::fromUtf8("TextFinder"));
+        TextFinder->resize(400, 300);
+        widget = new QWidget(TextFinder);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(30, 10, 270, 225));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        label = new QLabel(widget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        horizontalLayout->addWidget(label);
+
+        lineEdit = new QLineEdit(widget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+        findButton = new QPushButton(widget);
+        findButton->setObjectName(QString::fromUtf8("findButton"));
+
+        horizontalLayout->addWidget(findButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        textEdit = new QTextEdit(widget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+
+        verticalLayout->addWidget(textEdit);
+
+
+        retranslateUi(TextFinder);
+
+        QMetaObject::connectSlotsByName(TextFinder);
+    } // setupUi
+
+    void retranslateUi(QWidget *TextFinder)
+    {
+        TextFinder->setWindowTitle(QApplication::translate("TextFinder", "TextFinder", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("TextFinder", "Keyword:", 0, QApplication::UnicodeUTF8));
+        findButton->setText(QApplication::translate("TextFinder", "Find", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class TextFinder: public Ui_TextFinder {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_TEXTFINDER_H
